1e102057825831ec89f2ea04dda43e25c9d49794
Vladyslav Tyshchenko <Dvados9989@gmail.com>
2024-05-15 22:15:32 -0600

    Accessing More Properties

--- src/App.js
+++ src/App.js
@@ -5,19 +5,30 @@ import lottery from "./lottery";
 class App extends Component {
 
   state = {
-    manager: ''
+    manager: '',
+    players: [],
+    balance: ''
   };
 
   async componentDidMount() {
-    const manager = await lottery.methods.manager().call();
-    this.setState({ manager });
+    const [manager, players, balance] = await Promise.all([
+      lottery.methods.manager().call(),
+      lottery.methods.getPlayers().call(),
+      web3.eth.getBalance(lottery.options.address)
+    ]);
+
+    this.setState({ manager, players, balance });
   }
 
   render() {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by {this.state.manager}</p>
+        <p>
+          This contract is managed by {this.state.manager} 
+          There are currently {this.state.players.length} people entered, 
+          competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+        </p>
       </div>
     );
   }
