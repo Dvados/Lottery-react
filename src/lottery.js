83e6a9380932ee33e98c4cec165f1feb87c7093f
Vladyslav Tyshchenko <Dvados9989@gmail.com>
2024-05-16 13:02:26 -0600

    Adding new contract address and abi

--- src/lottery.js
+++ src/lottery.js
@@ -1,58 +1,45 @@
 import web3 from "./web3";
 
-const address = '0x3Bcc5f8b0deD20cfe3265a151D3137eeA2134968';
+const address = '0x213cECBF79E9EC9D285582e800b81c4e2F214150';
 
 const abi = [
+  { inputs: [], stateMutability: "nonpayable", type: "constructor" },
   {
-    constant: true,
-    inputs: [],
-    name: "manager",
-    outputs: [{ name: "", type: "address" }],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [],
-    name: "pickWinner",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "getPlayers",
-    outputs: [{ name: "", type: "address[]" }],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: false,
     inputs: [],
     name: "enter",
     outputs: [],
-    payable: true,
     stateMutability: "payable",
     type: "function",
   },
   {
-    constant: true,
-    inputs: [{ name: "", type: "uint256" }],
-    name: "players",
-    outputs: [{ name: "", type: "address" }],
-    payable: false,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [
+      { internalType: "address payable[]", name: "", type: "address[]" },
+    ],
     stateMutability: "view",
     type: "function",
   },
   {
     inputs: [],
-    payable: false,
+    name: "manager",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
     stateMutability: "nonpayable",
-    type: "constructor",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ internalType: "address payable", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
   },
 ];
 
