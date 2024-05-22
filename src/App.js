b494391d983c43965f82b110a713e42cc6459010
Vladyslav Tyshchenko <Dvados9989@gmail.com>
2024-05-22 12:17:01 -0600

    Quick update

--- src/App.js
+++ src/App.js
@@ -55,8 +55,8 @@ class App extends Component {
 
         <h2>Lottery Contract</h2>
         <p>
-          This contract is managed by {this.state.manager} 
-          There are currently {this.state.players.length} people entered, 
+          This contract is managed by {this.state.manager}
+          &nbsp;There are currently {this.state.players.length} people entered, 
           competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
         </p>
 
